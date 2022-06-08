821da24f80d93319198cf76cf2a8aa4a419a9290
MarcoMR4 <101607091+MarcoMR4@users.noreply.github.com>
2022-06-08 16:54:14 -0500

    agregando fondo a index

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ Grupo   : C
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-primary">
+    <body style="background-image:url('img/principal.jpg')">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
@@ -61,20 +61,7 @@ Grupo   : C
                     </div>
                 </main>
             </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
+        
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
