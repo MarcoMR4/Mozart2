cae691f4813999a2c6bd721f4a3786a16248e26f
MarcoMR4 <101607091+MarcoMR4@users.noreply.github.com>
2022-06-09 15:10:14 -0500

    acomodar encabezado

--- principal.php
+++ principal.php
@@ -28,32 +28,33 @@ if(!isset($_SESSION['username'])){
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-          <div class="row">
-            <a class="navbar-brand ps-3 col-sm" href="principal.php">
+            <div class="container">
+                <div class="row">
+                <div class="col">
+                 <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 col-sm" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+                 </div>
+                 <div class="col">
+                 <a class="navbar-brand col-sm" href="principal.php">
                   <img src="img/icono_mozart.png"  width="70px" alt="profile">
                   Conservatorio Mozart
-            </a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 col-sm" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-   
-            <!-- Navbar-->
-            <div class=" col-sm" style="">
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                 </a>
+                 </div>
+                 <div class=" col-sm">
+             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="editUser.php">Editar datos</a></li>
                         <li><a class="dropdown-item" href="cerrar.php">Cerrar sesión</a></li>
                     </ul>
-                   
                 </li>
             </ul>
+            </div>
+                </div>
+               <div class="row">
 
             </div>
-            
-
-
-          </div>
+       </div>
            
         </nav>
         <div id="layoutSidenav">
